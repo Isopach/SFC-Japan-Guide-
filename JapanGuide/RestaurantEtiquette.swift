3fa34c6e16b2df9ef2c50871455c23f040bd4239
You_liang Koh <kohyouliang@gmail.com>
2016-11-04 11:31:16 +0900

    add button link

---
+++ JapanGuide/RestaurantEtiquette.swift
@@ -0,0 +1,38 @@
+//
+//  RestaurantEtiquette.swift
+//  JapanGuide
+//
+//  Created by You_liang Koh on 2016/11/04.
+//  Copyright © 2016年 SFC. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantEtiquette: UIView {
+// MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+        //button.backgroundColor = UIColor.redColor()
+        
+        addSubview(button)
+
+    }
+    
+    
+    override func intrinsicContentSize() -> CGSize {
+        return CGSize(width: 240, height: 44)
+    }
+    
+// MARK: Button Action
+    
+    
+    func restaurantButtonTapped(button: UIButton) {
+        print("Button pressed 👍")
+    }
+    
+
+
+}
